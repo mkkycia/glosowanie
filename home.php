781d7109fcc66746dd9292a0f44b9c3d755345cb
Marek Kycia <marek24@boss.staszic.waw.pl>
2023-05-31 21:48:35 +0200

    cos

--- home.php
+++ home.php
@@ -1,18 +1,136 @@
 <HTML>
     <HEAD>
 <link href="https://www.ukodowani.pl/favicon.ico" rel="icon" type="image/x-icon">
+<audio src="https://vgmsite.com/soundtracks/minecraft/npdaqyidiz/Volume%20Beta%2004.%20Blind%20Spots.mp3" autoplay></audio>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+	<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>	
+
+	<script>
+  $( function() {
+	var availableNames = [
+		"Alexandrowicz Monika",
+        "Antonkiewicz Nina",
+        "Bartoszek Krzysztof",
+        "Bechler Paweł",
+        "Białoszewska Renata",
+        "Białoszewski Paweł",
+        "Bogdanowicz Joanna",
+        "Brzostek Aldona",
+        "Buratyński Wiktor",
+        "Chludzińska Beata",
+        "Chrząstowski – Wachtel Piotr",
+        "Cieślak Piotr",
+        "Czekaj Milena",
+        "Danielak Beata",
+        "Domaradzki Kamil",
+        "Drabot Katarzyna",
+        "Drąg Gabriel",
+        "Dzwonowska Urszula",
+        "Farbiś Karol",
+        "Gawrzak Małgorzata",
+        "Gładczuk Jolanta",
+        "Gładczuk Leszek",
+        "Golańska Marta",
+        "Górecka Edyta",
+        "Heropolitańska - Janik Joanna",
+        "Jacher-Cebula Anna",
+        "Jędral Jarosław",
+        "Jędrasik Maciej",
+        "Jungrav Wiera",
+        "Juszczak Ewa",
+        "Kaczyński Arkadiusz",
+        "Kajkowska Joanna",
+        "Kamiński Łukasz",
+        "Kardaś Michał",
+        "Karpiński Szymon",
+        "Kierzkowski Karol",
+        "Kimizuka Wiesława",
+        "Kołakowska-Przybyłek Zuzanna",
+        "Konarski Jerzy",
+        "Korzan Kamil",
+        "Koza Joanna",
+        "ks. Ośko Marcin",
+        "Kułagowska Anna",
+        "Kuś Agnieszka",
+        "Lann vel Lace Małgorzata",
+        "Lenkiewicz Martyna",
+        "Lewkowicz Regina",
+        "Lorek Krzysztof",
+        "Marciniuk Katarzyna",
+        "Markiewicz Natalia",
+        "Markowska Agata",
+        "Martys Wojciech",
+        "Mazurek Monika",
+        "Michalska Katarzyna",
+        "Mikos Anna",
+        "Moryc-Stańczyk Magdalena",
+        "Myziak Michał",
+        "Nosowska Dorota",
+        "Nowak Andrzej",
+        "Nowak Jakub",
+        "Okulska-Chmielewska Ewa",
+        "Pachulski Jan",
+        "Pawlikowski Jakub",
+        "Pawłowski Marek",
+        "Pluta Małgorzata",
+        "Pompe Waldemar",
+        "Potocka Agnieszka",
+        "Potocki Janusz",
+        "Regiec Dominika",
+        "Rong Joanna",
+        "Rotkiewicz Mikołaj",
+        "Rutkowska Anna",
+        "Sadowska Monika",
+        "Samulak Anna",
+        "Sidz Leszek",
+        "Socha Katarzyna",
+        "Staniewicz Paweł",
+        "Szydłowski Andrzej",
+        "Szypuła Katarzyna",
+        "Śmigielska Joanna",
+        "Świtalska Agnieszka",
+        "Tokarska -Serwa Agnieszka",
+        "Tsangaras Maria",
+        "Wanago-Wojtczak Beata",
+        "Ważna Maria",
+        "Winter Tomasz",
+        "Wiśniewska Magdalena",
+        "Wiśniewska Zofia",
+        "Witecki Arkadiusz",
+        "Wolsan Jadwiga",
+        "Zarzycki Sławomir",
+        "Zielicz Włodzimierz",
+        "Zieliński Maciej",
+        "Zieliński Przemysław",
+        "Ziemiańska Olga",
+        "Zimnoch Jolanta",
+		"Zmysłowski Adam"
+	];
+	$( "#tags" ).autocomplete({
+		source: availableNames		
+	});
+  } );
+  </script>
 
     </HEAD>
 
 <BODY>
-    <?php
-        session_start();
-        if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-            header("Location: test.php");
-            exit();
-		}
-		echo "jestes zalogowany jako " . $_SESSION["username"];
-        echo "cos zedytowalem"
-    ?>
+<?php
+	session_start();
+	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+		header("Location: test.php");
+		exit();
+	}
+	echo "jestes zalogowany jako " . $_SESSION["username"];
+?>
+
+<form action="dajglos.php" method="post">
+<h2 style="font-family: Arial">Zaglosuj na najlepszego nauczyciela!</h2>
+<input type="text" id="tags" name="nazwa" placeholder="Nazwa"><br>
+<button type="submit">wyslij</button>
+</form>
+
+
 </BODY>
 </HTML>
--- rejestracja.php
+++ rejestracja.php
@@ -3,22 +3,21 @@
 <audio src="https://vgmsite.com/soundtracks/minecraft/esiyjzozpe/Volume%20Alpha%2008.%20Minecraft.mp3" autoplay></audio>
 <link href="https://www.ukodowani.pl/favicon.ico" rel="icon" type="image/x-icon">
 
-<link rel="shortcut icon" src="http://potencjalkobiet.pl/img/rada/lewkowicz.jpg">
 <title> Rejestracja </title>
 </head>
 <link rel="stylesheet" type="text/css" href="style2.css">
 <body>
  <form action="verify.php" method="post">
-<h2>REJESTRACJA</h2>
+<h2 style="font-family: Arial">REJESTRACJA</h2>
  <?php if (isset($_GET['error'])) { ?>
   <p class="error"><?php echo $_GET['error']; ?></p>
  <?php } ?>
-<label>Nazwa Uzytkownika</label>
-<input type="text" name="uname" placeholder="User Name"><br>
-<label>Haslo</label>
-<input type="password" name="password" placeholder="Password"><br>
-<button type="submit">REGISTER</button>
-<a href="index.php">powrot to logowania</a>
+<label style="font-family: Arial">Nazwa Uzytkownika</label>
+<input type="text" name="uname" placeholder="User Name"label style="font-family: Arial"><br>
+<label style="font-family: Arial">Haslo</label>
+<input type="password" name="password" placeholder="Password"label style="font-family: Arial"><br>
+<button type="submit"label style="font-family: Arial">REGISTER</button>
+<a href="index.php" style="font-family: Arial">powrot to logowania</a>
 </form>
 
 </body>
